afe2b91ec1a1579dd187fc6f00be15a13912ad14
vmulu <vmulu@bu.edu>
2025-11-04 13:04:31 -0500

    added error handling

--- ssr/GetCats.ts
+++ ssr/GetCats.ts
@@ -1,8 +1,20 @@
+// stops api from being exposed to client
 "use server"
+
 const API_KEY=process.env.CAT_API_KEY;
 
 export default async function getCats(){
     const res= await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${API_KEY}`);
+
+    //  exceed your daily API-call limit
+    if (res.status === 429) {
+      throw new Error("RATE_LIMIT: Exceeded your daily Cat API limit. Try again later.");
+    }
+
+    // API is down
+    if (res.status >= 500) {
+      throw new Error("API_DOWN: The Cat API is temporarily unavailable. Please try again soon.");
+    }
 
     if (!res.ok) {
         throw new Error(`Fetch failed: ${res.status} ${res.statusText}`);
